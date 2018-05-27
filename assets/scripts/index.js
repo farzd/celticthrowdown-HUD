b90b6dca204745a3bad469823d6a456f93798545
farzd <qasimf@tcd.ie>
2018-05-27 17:02:48 +0100

    Commentary animations

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,11 +5,16 @@ function addClass(el, className) {
 
 var panelL = document.querySelectorAll('.panelL')[0];
 var panelR = document.querySelectorAll('.panelR')[0];
-var panelT = document.querySelectorAll('.panelT')[0];
+//var panelT = document.querySelectorAll('.panelT')[0];
 var logo = document.querySelectorAll('.logo')[0];
+var triangle = document.querySelectorAll('.triangle')[0];
+
+console.log({triangle});
+
 document.getElementsByTagName('body')[0].addEventListener('click' , function () {
     addClass(panelL, 'animate_pan')
     addClass(panelR, 'animate_pan')
-    addClass(panelT, 'animate_panT')
+    //addClass(panelT, 'animate_panT')
     addClass(logo, 'animate_logo')
+    addClass(triangle, 'animate_triangle')
 });
